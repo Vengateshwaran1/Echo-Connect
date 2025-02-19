81c2bb8227612cfc7b6da01a5a6f6617818252c9
Vengateshwaran1 <kvengateshwaran1@gmail.com>
2025-02-19 09:51:46 +0530

    commit

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,6 +1,5 @@
 import Topbar from "../../components/Topbar.tsx"
 import Boy from "../../components/Boy.tsx"
-import Girl from "../../components/Girl.tsx"
 const HomePage = () => {
   return (
     <div>
