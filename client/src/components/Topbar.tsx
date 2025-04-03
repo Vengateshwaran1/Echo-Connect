039b93ae3160880ae6cfdb733420806076bd665e
Vengateshwaran1 <kvengateshwaran1@gmail.com>
2025-04-03 09:21:53 +0530

    commit

--- client/src/components/Topbar.tsx
+++ client/src/components/Topbar.tsx
@@ -7,7 +7,7 @@ import { useAuthStore } from "@/stores/useAuthStore";
 const Topbar = () => {
   const isAdmin = useAuthStore();
   return (
-    <div className="flex items-center justify-between p-4 sticky top-0 border border-purple-500/20 bg-black/40 backdrop-blur-md z-10 rounded-lg">
+    <div className="flex items-center justify-between p-4 sticky top-0 border border-purple-800 bg-black/40 backdrop-blur-md z-10 rounded-lg">
         <div className="flex items-center gap-3 px-2 mb-8  cursor-pointer mt-4">
           <BoomBox className="h-8 w-8 text-purple-400" />
           <h1 className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-500">
@@ -16,7 +16,7 @@ const Topbar = () => {
         </div>
       <div className="flex items-center gap-4">
         {isAdmin && (
-          <Link to={"/admin"} className="flex items-center rounded-xl bg-gradient-to-t from-gray-400  p-1 text-sm border hover:border-gray-400 hover:scale-105">
+          <Link to={"/admin"} className="flex items-center rounded-xl bg-gradient-to-t from-purple-950 to-pink-400  p-1 text-sm border hover:border-gray-400 hover:scale-105">
             <LayoutDashboardIcon className="size-5 mr-2" />
               Admin dashboard
           </Link>
